7d4358ed175631cffec5f72bf7ca83c1118baaba
cinema7 <raitech2015@gmail.com>
2021-11-28 11:11:14 +0330

    first commit2

---
+++ precache-manifest.ca6b88f6c8dad98ab4b29d1f6216ccef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed86e7efaf2434f623bd462f63746e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27730864fe364b36788c",
+    "url": "/static/css/main.51772d8f.chunk.css"
+  },
+  {
+    "revision": "ba0b6e316275b710cbf5",
+    "url": "/static/js/2.678d1fe2.chunk.js"
+  },
+  {
+    "revision": "5031091b1f7f6986d4a47b9d37f83b0c",
+    "url": "/static/js/2.678d1fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27730864fe364b36788c",
+    "url": "/static/js/main.2a9ec771.chunk.js"
+  },
+  {
+    "revision": "11e218efb5c51d81050a",
+    "url": "/static/js/runtime-main.674690c8.js"
+  }
+]);
